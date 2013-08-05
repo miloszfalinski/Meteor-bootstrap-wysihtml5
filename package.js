3fc659dc7eeaeb23358c5aac05a4e43d46a019d6
Milosz Falinski <miloszfalinski@gmail.com>
2013-08-05 08:57:37 +0100

    Initial Commit

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+    summary: "Port of Bootstrap-wysihtml5 to Meteor."
+});
+
+Package.on_use(function (api) {
+    api.use('bootstrap', 'client');
+    api.add_files([
+        'lib/wysihtml5-0.3.0.min.js',
+        'lib/bootstrap-wysihtml5.js',
+        'lib/bootstrap-wysihtml5.css',
+        'lib/wysiwyg-color.css'
+    ], 'client');
+});
