4ccac583ba53fa4e5ce45e3badfd1df708f6341b
Milosz Falinski <miloszfalinski@gmail.com>
2013-08-05 10:56:47 +0100

    Fixed working with Meteor.
    Bootstrap plugin was relying on global variable from a separately
    loaded file - Meteor packages each file as an anonmymous function, so
    the plugin couldn't call the window.wysihtml5.
    
    Moved them both to a single file.

--- package.js
+++ package.js
@@ -5,9 +5,6 @@ Package.describe({
 Package.on_use(function (api) {
     api.use(['bootstrap', 'jquery'], 'client');
     api.add_files([
-        'lib/_wysihtml5-0.3.0.min.js'
-    ], 'client');
-    api.add_files([
         'lib/bootstrap-wysihtml5.js',
         'lib/bootstrap-wysihtml5.css',
         'lib/wysiwyg-color.css'
