1e2fb76a5176ac99c0acb0dcf401dcb36e684682
Milosz Falinski <miloszfalinski@gmail.com>
2013-08-05 09:57:22 +0100

    Hopefully fixes the whole thing, this one.

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function (api) {
     api.use(['bootstrap', 'jquery'], 'client');
     api.add_files([
-        'lib/wysihtml5-0.3.0.min.js',
+        'lib/_wysihtml5-0.3.0.min.js',
         'lib/bootstrap-wysihtml5.js',
         'lib/bootstrap-wysihtml5.css',
         'lib/wysiwyg-color.css'
